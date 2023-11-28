3ee802253b06e77e9c3a30d1d95706583cc36cec
Coppermine-SP <son5121502@gmail.com>
2023-11-28 23:19:52 +0900

    fix: scroll이 top이 아닌 상태에서 로드시, nav가 no-shadow 상태로 시작함.

--- photo_gallery/static/js/header.js
+++ photo_gallery/static/js/header.js
@@ -1,12 +1,15 @@
 //매뉴가 슬라이더 그라디언트에 있을 때, 그림자 제거로 그라디언트와 일체화.
-$(document).ready(function () {
-    $('nav').addClass('no-shadow');
-});
-
-$(window).on('scroll', function () {
+function dynamicNav() {
     if ($(window).scrollTop() > 10) {
         $('nav').removeClass('no-shadow');
     } else {
         $('nav').addClass('no-shadow');
     }
+}
+$(document).ready(function () {
+    dynamicNav();
+});
+
+$(window).on('scroll', function () {
+    dynamicNav();
 });
