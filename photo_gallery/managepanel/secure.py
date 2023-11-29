2e99759591627e78fe57536e8a3dda163af3f7d9
Coppermine-SP <son5121502@gmail.com>
2023-11-29 21:42:34 +0900

    refactor: secure 모듈화

--- photo_gallery/managepanel/secure.py
+++ photo_gallery/managepanel/secure.py
@@ -6,6 +6,8 @@ ALLOWED_IP_LIST = ["127.0.0.1", "10.*.*.*"]
 
 
 # IP-based access control.
+# 배포 환경에서 안전하지 않음: 변조된 HTTP 헤더를 구분할 수 없음.
+# 배포 환경에서는 반드시 신뢰할 수 있는 리버스 프록시에서 X-Forwarded-For 헤더를 통해 발신지를 확인할 것.
 def allowed_ips(allowed_ip):
     def decorator(func):
         @wraps(func)
