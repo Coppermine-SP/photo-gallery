95f563cd9618b17fec0b6f7cd092141580c8267d
Coppermine-SP <son5121502@gmail.com>
2023-12-02 02:34:35 +0900

    feat: upload POST 동작 추가

--- photo_gallery/managepanel/views.py
+++ photo_gallery/managepanel/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.paginator import Paginator
+from django.views.decorators.clickjacking import xframe_options_sameorigin
 from .secure import *
 from photo_gallery.models import photo_metadata
 
@@ -15,3 +16,11 @@ def manage(request):
     else:
         return redirect("/error?id=404")
     return render(request, "managepanel/manage.html", {"photoList": photo_list, "category" : cat, "rowCount" : len(photo_list)})
+
+@allowed_ips(ALLOWED_IP_LIST)
+@xframe_options_sameorigin
+def upload(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, "managepanel/upload.html")
