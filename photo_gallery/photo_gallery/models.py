50dc9f11e6d9f17ab464b6e804a7e30880d987f9
Coppermine-SP <son5121502@gmail.com>
2023-12-03 00:37:03 +0900

    feat: 내부 카테고리 이름 포맷팅 함수 추가

--- photo_gallery/photo_gallery/models.py
+++ photo_gallery/photo_gallery/models.py
@@ -2,13 +2,20 @@ from django.db import models
 from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFit
 
+
 # Create your models here.
 class photo_metadata(models.Model):
     description = models.TextField(blank=True)
     category = models.TextField()
     published_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to="img")
-    thumbnail = ImageSpecField(source='image', processors=[ResizeToFit(800, 600)],format='JPEG', options={'quality' : 60})
+    thumbnail = ImageSpecField(source='image', processors=[ResizeToFit(800, 600)], format='JPEG',
+                               options={'quality': 60})
 
     def __str__(self):
         return self.id
+
+    @property
+    def category_format(self):
+        dic = {"landscape": "Landscapes", "portrait": "Portraits", "innovation": "Innovations"}
+        return dic.get(self.category, "-")
