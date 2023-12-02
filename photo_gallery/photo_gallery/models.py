1e4f4786c2cccecbe953af6c0699b2a92cc84c4a
Coppermine-SP <son5121502@gmail.com>
2023-12-02 20:48:08 +0900

    feat: 이미지 썸내일 지원 추가

--- photo_gallery/photo_gallery/models.py
+++ photo_gallery/photo_gallery/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFit
 
 # Create your models here.
 class photo_metadata(models.Model):
     description = models.TextField(blank=True)
     category = models.TextField()
-    published_at = models.DateTimeField()
-    image = models.ImageField()
+    published_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="img")
+    thumbnail = ImageSpecField(source='image', processors=[ResizeToFit(800, 600)],format='JPEG', options={'quality' : 60})
 
     def __str__(self):
         return self.id
