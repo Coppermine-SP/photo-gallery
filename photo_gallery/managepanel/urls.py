152690110494adc1d701c6f9f1744cb235cc9db5
Coppermine-SP <son5121502@gmail.com>
2023-12-03 21:27:19 +0900

    feat: 레코드 삭제 기능 추가

--- photo_gallery/managepanel/urls.py
+++ photo_gallery/managepanel/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "managepanel"
 urlpatterns = [
     path('manage', views.manage, name='manage'),
-    path('upload', views.upload, name='upload')
+    path('upload', views.upload, name='upload'),
+    path('delete', views.delete, name='delete')
 ]
--- photo_gallery/managepanel/views.py
+++ photo_gallery/managepanel/views.py
@@ -15,7 +15,7 @@ def manage(request):
         photo_list = photo_metadata.objects.filter(category=cat)
     else:
         return redirect("/error?id=404")
-    return render(request, "managepanel/manage.html", {"photoList": photo_list, "category" : cat, "rowCount" : len(photo_list)})
+    return render(request, "managepanel/manage.html", {"photoList": photo_list, "category" : cat, "rowCount" : len(photo_list), "ip" : request.META.get("REMOTE_ADDR"), "user_agent" : request.META.get("HTTP_USER_AGENT"), "is_secure" : request.is_secure()})
 
 @allowed_ips(ALLOWED_IP_LIST)
 @xframe_options_sameorigin
@@ -34,3 +34,19 @@ def upload(request):
             else:
                 return redirect("/error?id=400")
         return redirect("/error?id=400")
+
+
+@allowed_ips(ALLOWED_IP_LIST)
+def delete(request):
+    if request.method != 'POST':
+        return redirect("/error?id=400")
+    else:
+        target_id = request.POST.get('id')
+        print(target_id)
+        try:
+            target = photo_metadata.objects.get(id=target_id)
+            target.delete()
+        except(e):
+            print(e)
+            return HttpResponse(status=500)
+        return HttpResponse()
